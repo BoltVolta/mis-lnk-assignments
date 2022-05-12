b1d7aa97260b06bd3f3107868cdc6ecbde90e08b
Daniil Kovalev <61245613+BoltVolta@users.noreply.github.com>
2022-05-12 10:44:32 +0300

    Create index.js

---
+++ assignment8/index.js
@@ -0,0 +1,9 @@
+
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import TableData from './Jokes';
+
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<TableData />)
